45aa3e9ce99c3813d5c36cefe8f1d580bec254a2
Basant Bhandari <36443209+basantbhandari@users.noreply.github.com>
2020-05-13 22:30:38 +0545

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject winText;
+    public GameObject reStartButton;
+    int score = 0;
+
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //to change the score
+
+   public void ScoreUp()
+    {
+        score++;
+        if (score >= 5) {
+            Win();
+        }
+    }
+
+    //when all the balls get destroyed
+    void Win() {
+
+        winText.SetActive(true);
+        reStartButton.SetActive(true);
+
+    }
+
+
+    //in order to restart the game
+    public void Restart() {
+        SceneManager.LoadScene("Game");
+
+    }
+
+
+
+
+
+
+}
